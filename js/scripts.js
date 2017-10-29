19397141c442360ee2d0011e8916574090bb7110
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2017-10-29 11:19:33 -0700

    Add project files and contact data will display on right column

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+//Business logic
+
+function Contact(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+// User Interface
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+
+//creates a new Contact object by calling the Contact constructor and passing it the inputted data
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+//show the contact information when it is clicked. By adding last(), we only bind the event to most recently-inserted contact.
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+  });
+});
