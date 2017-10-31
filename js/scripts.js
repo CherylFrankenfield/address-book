b145b052e97e2286353f9e5b4f0270c2866482fa
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2017-10-30 21:42:53 -0700

    Add another button and address frontend info

--- js/scripts.js
+++ js/scripts.js
@@ -19,14 +19,39 @@ Contact.prototype.fullName = function() {
 
 // User Interface
 $(document).ready(function() {
+  //if the user wants to add two different addresses, they'll need to be able to hit the "Another Address" button to receive more address form fields before submitting the form
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Street</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City</label>' +
+                                   '<input type="text" class="form-control new-city">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-state">State</label>' +
+                                   '<input type="text" class="form-control new-state">' +
+                                 '</div>' +
+                               '</div>');
+  });
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
-
 //creates a new Contact object by calling the Contact constructor and passing it the inputted data
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+//a loop that cycles through each DOM element with the class new-address
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find("input.new-street").val();
+      var inputtedCity = $(this).find("input.new-city").val();
+      var inputtedState = $(this).find("input.new-state").val();
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState)
+      newContact.addresses.push(newAddress)
+    });
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 //show the contact information when it is clicked. By adding last(), we only bind the event to most recently-inserted contact.
@@ -35,9 +60,16 @@ $(document).ready(function() {
       $("#show-contact h2").text(newContact.firstName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
+      $("ul#addresses").text("");
+      newContact.addresses.forEach(function(address) {
+        $("ul#addresses").append("<li>" + address.street + ", " + address.city + " " + address.state + "</li>");
+      });
     });
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-state").val("");
   });
 });
